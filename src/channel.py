ae3889aad83793d8f00d15854ac74458673dcf6c
Andry Vilkov <vilkov-l-nn@mail.ru>
2023-12-26 20:35:12 +0300

    edit2 channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,10 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -9,4 +16,5 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
