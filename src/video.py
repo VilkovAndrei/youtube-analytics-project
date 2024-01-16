c7c5e1a8a25d32f6c5aca40422e6e7f5220e1b8a
Andry Vilkov <vilkov-l-nn@mail.ru>
2024-01-16 23:36:28 +0300

    Add video.py class PLVideo

--- src/video.py
+++ src/video.py
@@ -20,7 +20,6 @@ class Video:
 
         video_dict = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                            id=self.video_id).execute()
-        print(video_dict)
         self.video_title: str = video_dict['items'][0]['snippet']['title']
         self.view_count: int = int(video_dict['items'][0]['statistics']['viewCount'])
         self.like_count: int = int(video_dict['items'][0]['statistics']['likeCount'])
@@ -29,11 +28,35 @@ class Video:
     def __str__(self):
         return f"{self.video_title}"
 
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о видео."""
+        video_dict = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                           id=self.video_id).execute()
+        print(json.dumps(video_dict, indent=2, ensure_ascii=False))
+
     def to_json(self, filename):
         video_info = {"video_title": self.video_title,
                       "video_id": self.video_id,
                       "video_url": self.video_url,
                       "view_count": self.view_count,
                       "like_count": self.like_count}
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(video_info, file, indent=4, ensure_ascii=False)
+
+
+class PLVideo(Video):
+    """Класс для ютуб-видео + плей-лист"""
+
+    def __init__(self, str_video_id: str, str_playlist_id: str):
+        super().__init__(str_video_id)
+        self.playlist_id = str_playlist_id
+
+    def to_json(self, filename):
+        video_info = {"video_title": self.video_title,
+                      "video_id": self.video_id,
+                      "video_url": self.video_url,
+                      "view_count": self.view_count,
+                      "like_count": self.like_count,
+                      "playlist_id": self.playlist_id}
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(video_info, file, indent=4, ensure_ascii=False)
